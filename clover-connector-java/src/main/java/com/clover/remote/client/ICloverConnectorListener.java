d02eaf8131e3112d7c0577db84946771884cbcd2
Kate-at-Clover <kate@clover.com>
2017-11-14 16:28:39 -0700

    Started standardizing doc text

--- clover-connector-java/src/main/java/com/clover/remote/client/ICloverConnectorListener.java
+++ clover-connector-java/src/main/java/com/clover/remote/client/ICloverConnectorListener.java
@@ -54,80 +54,121 @@ import com.clover.remote.message.TipAddedMessage;
 public interface ICloverConnectorListener {
 
   /**
-   * Called when a Clover device activity starts.
+   * Called when the Clover device transitions to a new screen or activity. The 
+   * CloverDeviceEvent passed in will contain an event type, a description, and a list of 
+   * available InputOptions.
    *
    * @param deviceEvent The device event.
    */
   void onDeviceActivityStart(CloverDeviceEvent deviceEvent);
 
   /**
-   * Called when a Clover device activity ends.
+   * Called when the Clover device transitions away from a screen or activity. The 
+   * CloverDeviceEvent passed in will contain an event type and description. 
+   *
+   * <p>
+   * <b>Note:</b> The start and end events are not guaranteed to process in order. The 
+   * event type should be used to make sure these events are paired.
    *
    * @param deviceEvent The device event.
    */
   void onDeviceActivityEnd(CloverDeviceEvent deviceEvent);
 
   /**
-   * Called when a Clover device error event is encountered
+   * Called when an error occurs while trying to send messages to the Clover device.
    *
    * @param deviceErrorEvent The device error event.
    */
   void onDeviceError(CloverDeviceErrorEvent deviceErrorEvent);
 
   /**
-   * Called in response to a pre auth request
+   * Called in response to a PreAuth() request. 
    *
-   * @param response The response
+   * <p>
+   * <b>Note:</b> The boolean isPreAuth flag in the PreAuthResponse indicates whether 
+   * CapturePreAuth() can be called for the returned Payment. If the isPreAuth flag is 
+   * false and the isAuth flag is true, then the payment gateway coerced the PreAuth() 
+   * request to an Auth. 
+   * The payment will need to be voided or it will be automatically captured at closeout.
+   *
+   * @param response The response to the transaction request.
    */
   void onPreAuthResponse(PreAuthResponse response);
 
   /**
-   * Called in response to an auth request
+   * Called in response to an Auth() request. 
    *
-   * @param response The response
+   * <p>
+   * <b>Note:</b> An Auth transaction may come back as a final Sale, depending on the 
+   * payment gateway. 
+   * The AuthResponse has a boolean isAuth flag that indicates whether the Payment can 
+   * still be tip-adjusted.
+   *
+   * @param response The response to the transaction request.
    */
   void onAuthResponse(AuthResponse response);
 
   /**
-   * Called in response to a tip adjust of an auth payment
+   * Called in response to a tip adjustment for an Auth transaction. 
+   * Contains the tipAmount if successful.
    *
-   * @param response The response
+   * @param response The response to the transaction request.
    */
   void onTipAdjustAuthResponse(TipAdjustAuthResponse response);
 
   /**
-   * Called in response to a capture of a pre auth payment
+   * Called in response to a CapturePreAuth() request. 
+   * Contains the new Amount and TipAmount if successful.
    *
-   * @param response The response
+   * @param response The response to the transaction request.
    */
   void onCapturePreAuthResponse(CapturePreAuthResponse response);
 
   /**
-   * Called when the Clover device requires a signature to be verified
+   * Called when the Clover device requests verification for a user's on-screen signature. 
+   * The Payment and Signature will be passed in.
    *
-   * @param request The request
+   * @param request The verification request.
    */
   void onVerifySignatureRequest(VerifySignatureRequest request);
 
   /**
-   * Called when the Clover device requires confirmation for a payment
-   * e.g. Duplicates or Offline
+   * Called when the Clover device encounters a Challenge at the payment gateway 
+   * and requires confirmation. A Challenge is triggered by a potential duplicate Payment 
+   * (DUPLICATE_CHALLENGE) or an offline Payment (OFFLINE_CHALLENGE). 
+   * The device sends a ConfirmPaymentRequest() asking the merchant 
+   * to either AcceptPayment() or RejectPayment().
    *
-   * @param request The request
+   * <p>
+   * <b>Note:</b> Duplicate Payment Challenges are raised when multiple Payments are made 
+   * with the same card type and last four digits within the same hour. For this reason, 
+   * we recommend that you do not programmatically call CloverConnector.RejectPayment() 
+   * on all instances of DUPLICATE_CHALLENGE. For more information, see 
+   * <a href="https://docs.clover.com/build/working-with-challenges/">Working with 
+   * Challenges</a>. 
+   *
+   * @param request The request for confirmation.
    */
   void onConfirmPaymentRequest(ConfirmPaymentRequest request);
 
   /**
-   * Called in response to a closeout being processed
+   * Called in response to a Closeout() request.
    *
-   * @param response The response
+   * @param response The response to the transaction request.
    */
   void onCloseoutResponse(CloseoutResponse response);
 
   /**
-   * Called in response to a sale request
+   * Called at the completion of a Sale() request. The SaleResponse contains a ResultCode 
+   * and a Success boolean. A successful Sale transaction will also have the payment 
+   * object, which can be for the full or partial amount of the Sale request. 
    *
-   * @param response The response
+   * <p>
+   * <b>Note:</b> A Sale transaction my come back as a tip-adjustable Auth, depending 
+   * on the payment gateway. The SaleResponse has a boolean isSale flag that indicates 
+   * whether the sale is final, or will be finalized during closeout.
+   *
+   * @param SaleResponse The response to the transaction request.
    */
   void onSaleResponse(SaleResponse response);
 
